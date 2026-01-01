e7f10579c98f76913d011cff675f85181216c4d3
Sumehar-Singh <savvy.grewal13@gmail.com>
2026-01-02 00:03:09 +0530

    style: change Home page SafeAreaView from red to white

--- screens/screens/guests/Home.js
+++ screens/screens/guests/Home.js
@@ -315,7 +315,7 @@ const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: '#ff0000',
+    backgroundColor: '#fff',
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
   container: {
