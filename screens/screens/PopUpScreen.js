4820be40569ecc4f86ce94f9830688c97d7759ad
Sumehar-Singh <savvy.grewal13@gmail.com>
2026-01-02 01:23:31 +0530

    style: updated PopUpScreen overlay to light gray

--- screens/screens/PopUpScreen.js
+++ screens/screens/PopUpScreen.js
@@ -73,7 +73,7 @@ export default PopUpScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
