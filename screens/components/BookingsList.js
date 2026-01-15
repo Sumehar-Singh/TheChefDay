d48a37e7658f89fbaddd022846e4117d474ba82d
Sumehar-Singh <savvy.grewal13@gmail.com>
2026-01-15 21:44:24 +0530

    style: sync 'Booked' text font weight to 500 to match 'Event' text

--- screens/components/BookingsList.js
+++ screens/components/BookingsList.js
@@ -367,7 +367,7 @@ const styles = StyleSheet.create({
     fontSize: isTablet ? 16 : 14,
     color: '#444', // Darker grey
     marginLeft: 8,
-    fontWeight: '700', // Made bold
+    fontWeight: '500', // Match Event weight
   },
   bookingTextService: {
     fontSize: isTablet ? 16 : 14,
